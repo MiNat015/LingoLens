1ed40bac50936e7ca626e37421df4fa049e0731c
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-08-26 13:19:21 +1000

    Initial Commit

---
+++ LingoLens/LingoLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoLensApp.swift
+//  LingoLens
+//
+//  Created by Arjun Srikanth on 26/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct LingoLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
