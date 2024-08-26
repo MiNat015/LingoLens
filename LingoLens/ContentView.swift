fa7db5bbed4b0dff5dfd731fa59b47b8773fec78
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-08-26 16:08:45 +1000

    Implemented text recognition in ContentView

--- LingoLens/ContentView.swift
+++ LingoLens/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import Vision
 
 struct ContentView: View {
+    @State private var cameraOpen = false
+    @State private var imageTaken : UIImage?
+    @State private var recognisedTexts = [String]()
+    @State private var isLoading = false
+    
+    func recogniseText() {
+        print("Reading Text")
+        let requestHandler = VNImageRequestHandler(cgImage: self.imageTaken!.cgImage!)
+        
+        let recognizeTextRequest = VNRecognizeTextRequest {(request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return
+            }
+            
+            for observation in observations {
+                let recognizedText = observation.topCandidates(1).first!.string
+                self.recognisedTexts.append(recognizedText)
+            }
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try requestHandler.perform([recognizeTextRequest])
+                
+                self.isLoading = false
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+    
+    var pictureTakenView: some View {
+        VStack {
+            Image(uiImage: self.imageTaken!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            Button(action: {
+                self.imageTaken = nil
+                self.recognisedTexts = [String]()
+            }, label: {
+                HStack {
+                    Image(systemName: "camera")
+                    Text("Re-take picture")
+                }
+            })
+            List {
+                ForEach(self.recognisedTexts, id: \.self) {
+                    Text("\($0)")
+                }
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if (self.imageTaken == nil) {
+                CameraView(image: self.$imageTaken)
+            } else {
+                if (!self.isLoading) {
+                    self.pictureTakenView
+                        .onAppear {
+                            self.recogniseText()
+                        }
+                } else {
+                    ProgressView()
+                }
+            }
         }
-        .padding()
     }
 }
 
