592e184e60984991e3fdfa320eddea3047bc5f64
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-08-27 00:09:44 +1000

    Added comments to the class

--- LingoLens/CameraView.swift
+++ LingoLens/CameraView.swift
@@ -9,11 +9,13 @@ import Foundation
 import SwiftUI
 import UIKit
 
+// UIViewControllerRepresentable to manage UIImagePickerController for taking pictures
 struct CameraView: UIViewControllerRepresentable {
     @Binding var image: UIImage?
     
     typealias UIViewControllerType = UIImagePickerController
     
+    // Create and configure UIImagePickerController
     func makeUIViewController(context: Context) -> UIViewControllerType {
         let viewController = UIViewControllerType()
         viewController.delegate = context.coordinator
@@ -26,6 +28,7 @@ struct CameraView: UIViewControllerRepresentable {
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
     
+    // Coordinator to handle UIImagePickerController delegate methods
     func makeCoordinator() -> CameraView.Coordinator {
         return Coordinator(self)
     }
@@ -41,21 +44,25 @@ extension CameraView {
             self.parent = parent
         }
         
+        // Handle cancel action in image picker
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             picker.dismiss(animated: true)
         }
         
+        // Resize the captured image to the target size
         func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
             return image.imageResized(to: targetSize)
         }
         
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) 
+        // Handle the image after it has been picked
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
         {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 print("Image Taken")
                 print("Width: " + image.size.width.description)
                 print("Height: " + image.size.height.description)
                 
+                // Resize the image and store it in the binding
                 self.parent.image = image.imageResized(to: CGSize(width: 3000, height:2250))
             }
         }
@@ -63,6 +70,7 @@ extension CameraView {
 }
 
 extension UIImage {
+    // Helper method to resize a given UIImage
     func imageResized(to size: CGSize) -> UIImage {
         return UIGraphicsImageRenderer(size:size).image { _ in
             draw(in: CGRect(origin: .zero, size: size))
